9a8644daac48d4b026e878f501cd7616c249b08a
Chanjong Baek <readandaction@gmail.com>
2020-09-01 12:26:44 +0900

    Set UserAdmin fieldsets

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
-    pass
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Detail",
+            {
+                "fields": (
+                    "bio",
+                    "avatar",
+                    "gender",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
