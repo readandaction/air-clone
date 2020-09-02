175731098a0b7682783a3c348d490a95a4a372f3
Chanjong Baek <readandaction@gmail.com>
2020-09-02 13:26:05 +0900

    Define Photo, HouseRule, Amenity ...

--- rooms/models.py
+++ rooms/models.py
@@ -4,9 +4,55 @@ from core import models as core_models
 from users import models as user_models
 
 
+class AbstactItem(core_models.TimeStampedModel):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstactItem):
+    """ RoomType Model definition """
+
+    class Meta:
+        verbose_name = "Room Type"
+
+
+class HouseRule(AbstactItem):
+    """ House Rule Model definition """
+
+    class Meta:
+        verbose_name = "House Rule"
+
+
+class Amenity(AbstactItem):
+
+    """ Amenity Model definition """
+
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+
+class Facility(AbstactItem):
+
+    """ facility Model definition """
+
+    class Meta:
+        verbose_name_plural = "Facilties"
+
+
+class Photo(core_models.TimeStampedModel):
+
+    """ Photo Model definition """
+
+    caption = models.CharField(max_length=30)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+
 class Room(core_models.TimeStampedModel):
 
-    "Room Model Definition"
+    """Room Model Definition"""
 
     name = models.CharField(max_length=40)
     description = models.TextField(null=True)
@@ -22,3 +68,10 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    room_type = models.ForeignKey(RoomType, null=True, on_delete=models.SET_NULL)
+    house_rules = models.ManyToManyField(HouseRule, blank=True)
+    amenities = models.ManyToManyField(Amenity, blank=True)
+    facilities = models.ManyToManyField(Facility, blank=True)
+
+    def __str__(self):
+        return self.name
