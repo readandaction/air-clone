164d063a2af67e5701f57b90a6d31c19a2d2baec
Chanjong Baek <readandaction@gmail.com>
2020-09-08 16:28:02 +0900

    Seed facilities

---
+++ rooms/management/commands/seed_facilities.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+from rooms.models import Facility
+
+
+class Command(BaseCommand):
+
+    help = "This command creates amenities"
+
+    def handle(self, *args, **options):
+        facilities = [
+            "Private entrance",
+            "Paid parking on premises",
+            "Paid parking off premises",
+            "Elevator",
+            "Parking",
+            "Gym",
+        ]
+
+        for a in facilities:
+            Facility.objects.create(name=a)
+        self.stdout.write(self.style.SUCCESS(f"{len(facilities)} facilites created"))
